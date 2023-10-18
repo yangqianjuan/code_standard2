0c1c8427a4350e812d3edbebd06e7cc9eac91b80
yangqianjuan <1072483500@qq.com>
2023-10-18 15:36:43 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,14 @@ commit_info=`git describe --all --always --long`
 dist_path=docs/.vuepress/dist
 push_banch=gh-pages
 
-npm run doc:build
+npm run docs:build
 
 cd $dist_path
 
 git init
 git add -A
 git commit -m "deploy,$commit_info"
+git push -f $push_addr HEAD:$push_banch
 
 cd -
 rm -rf $dist_path
